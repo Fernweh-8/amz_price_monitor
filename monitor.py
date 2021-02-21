653ab269e77098426b8024763735f6a10b9769cb
Fernweh-8 <unforgiving.void@gmail.com>
2021-02-21 22:11:49 +0100

    First commit
    Install bs4 and webdriver. Initialize an ebook list. Create Amazon URL for each ebook. Open the URL.

--- monitor.py
+++ monitor.py
@@ -1,1 +1,17 @@
-import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+ebook_list = []
+new_ebook = input("Enter the ebook's title: ")
+ebook_list.append(new_ebook)
+
+
+for ebook in ebook_list:
+    ebook = ebook.replace(" ", "+")
+    url = f"https://www.amazon.com/s?k={ebook}&ref=nb_sb_noss_2"
+    print(url)
+
+
+
+driver = webdriver.Chrome(r'A:Python\chromedriver_win32\chromedriver.exe')
+source = driver.get(url)
